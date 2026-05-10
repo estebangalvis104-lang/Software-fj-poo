746b991a6e72491c19c8f1836abc3737a0230545
estebangalvis104-lang <estebangalvis.104@gmail.com>
2026-05-10 14:13:34 -0500

    main.py
    Main conectado con clases

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-print("Proyecto Software FJ iniciado correctamente")
-#Primer archivo del proyecto
+from cliente import Cliente
+from sala import ReservaSala
+from reserva import Reserva
+
+try:
+
+    cliente1 = Cliente(
+        "Esteban",
+        "esteban@gmail.com"
+    )
+
+    servicio1 = ReservaSala(
+        "Sala VIP",
+        50000,
+        3
+    )
+
+    reserva1 = Reserva(
+        cliente1,
+        servicio1
+    )
+
+    reserva1.confirmar()
+
+    print(reserva1.mostrar_reserva())
+
+    print(
+        "Costo total:",
+        servicio1.calcular_costo()
+    )
+
+except Exception as e:
+    print("Ocurrió un error:", e)
